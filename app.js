da99addab676d1fbbacbb1fbaff5fe5ca6a27823
Cristian Abraham De Luna Mercado <crisistian87@gmail.com>
2024-06-13 17:53:47 -0600

    Tercer Comit

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express")
+const morgan = require('morgan')
 const app = express()
 const port = 3500
+const comidaRouter = require('./Routes/comidaRouter')
 
 app.get("/", (req, res)=>{
     res.json({mensaje: "Aplicacion funcionando Correctamente"})
 })
 
+app.use(morgan('dev'))
+app.use('/API', comidaRouter)
+
+//Lista blanca esto redirecciona al siguiente mensaje cuando acceden a algo inexistente
+app.use((req, res) => {
+    res.status(404).json({ message: 'Ruta no encontrada' });
+});
 
 app.listen(port, ()=>{
     console.log(`Aplicacion  corriendo en el puerto ${port}`)
 })
+
+
+
 
